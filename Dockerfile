0e9aea9323ec9903caaaad3f5a4fc57f237912f9
HS Coetzee <hs.coetzee@gmail.com>
2017-02-26 15:47:46 +0000

    Using headless, hope that will cut down on some noise installs.

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM armhf/ubuntu:16.04
 LABEL java="8" arch="armhf" supplies="jre"
 MAINTAINER HS Coetze <hs.coetzee+docker-java-armhf@gmail.com>
 RUN apt-get update
-RUN  apt-get install openjdk-8-jre -y && apt-get autoremove -y && apt-get clean -y && apt-get autoclean -y
+RUN  apt-get install openjdk-8-jre-headless -y && apt-get autoremove -y && apt-get clean -y && apt-get autoclean -y
 RUN update-alternatives --set java /usr/lib/jvm/java-8-openjdk-armhf/jre/bin/java
--- build-images.sh
+++ build-images.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-docker build -f Dockerfile -t demaniak/armhf-java-8 .
-docker tag demaniak/armhf-java-8 demaiak/armhf-java:8
-docker tag demaniak/armhf-java-8 demaniak/armhf-java:latest
-docker push demaniak/armhf-java:latest
-docker push demaniak/armhf-java:8
+IMAGE_NAME=demaniak/armhf-java
+docker build -f Dockerfile -t "$IMAGE_NAME"-8 .
+docker tag "$IMAGE_NAME"-8 "$IMAGE_NAME":8
+docker tag "$IMAGE_NAME"-8 "$IMAGE_NAME":latest
+docker push "$IMAGE_NAME":latest
+docker push "$IMAGE_NAME":8
 
-docker build -f Dockerfile-7 -t demaniak/armhf-java-7 .
-docker tag demaniak/armhf-java-7 demaniak/armhf-java:7
-docker push demaniak/armhf-java:7
+docker build -f Dockerfile-7 -t "$IMAGE_NAME"-7 .
+docker tag "$IMAGE_NAME"-7 "$IMAGE_NAME":7
+docker push "$IMAGE_NAME":7
